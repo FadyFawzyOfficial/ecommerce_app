ba3461ee5fcefe5672dcf579a2736203cb36e7e0
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-11-10 17:17:50 +0200

    5. Refactor ShoppingCartIcon to use GoRouter and add '/cart' route to app_router

--- ecommerce_app/lib/src/routing/app_router.dart
+++ ecommerce_app/lib/src/routing/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 
 import '../features/products_list/products_list_screen.dart';
+import '../features/shopping_cart/shopping_cart_screen.dart';
 
 final goRouter = GoRouter(
   initialLocation: '/',
@@ -9,6 +10,10 @@ final goRouter = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => const ProductsListScreen(),
+    ),
+    GoRoute(
+      path: '/cart',
+      builder: (context, state) => const ShoppingCartScreen(),
     ),
   ],
 );
