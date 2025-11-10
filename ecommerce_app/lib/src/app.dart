0105f3ea54c4a62777a3d5bfa19629120d584bc2
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-11-10 17:03:05 +0200

    3. Add initial location and debug logging to GoRouter configuration to see navigation logs

--- ecommerce_app/lib/src/app.dart
+++ ecommerce_app/lib/src/app.dart
@@ -10,6 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     final goRouter = GoRouter(
+      initialLocation: '/',
+      debugLogDiagnostics: true,
       routes: [
         GoRoute(
           path: '/',
