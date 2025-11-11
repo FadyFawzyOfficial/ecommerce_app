bc962ec6ac18920615110d446979e00e1c62c45f
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-11-11 15:05:36 +0200

    10. Refactor HomeAppBar and MoreMenuButton to use GoRouter; update app_router with orders, account, and sign-in routes (fullscreen dialogs)

--- ecommerce_app/lib/src/routing/app_router.dart
+++ ecommerce_app/lib/src/routing/app_router.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../features/account/account_screen.dart';
+import '../features/orders_list/orders_list_screen.dart';
 import '../features/products_list/products_list_screen.dart';
 import '../features/shopping_cart/shopping_cart_screen.dart';
+import '../features/sign_in/email_password_sign_in_screen.dart';
+import '../features/sign_in/email_password_sign_in_state.dart';
 
 final goRouter = GoRouter(
   initialLocation: '/',
@@ -17,6 +21,29 @@ final goRouter = GoRouter(
           pageBuilder: (context, state) => MaterialPage(
             fullscreenDialog: true,
             child: ShoppingCartScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'orders',
+          pageBuilder: (context, state) => MaterialPage(
+            fullscreenDialog: true,
+            child: OrdersListScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'account',
+          pageBuilder: (context, state) => MaterialPage(
+            fullscreenDialog: true,
+            child: AccountScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'signIn',
+          pageBuilder: (context, state) => MaterialPage(
+            fullscreenDialog: true,
+            child: EmailPasswordSignInScreen(
+              formType: EmailPasswordSignInFormType.signIn,
+            ),
           ),
         ),
       ],
