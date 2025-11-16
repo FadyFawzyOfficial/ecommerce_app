e22666be54f03f67150ce0cf47e43b8061d07041
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-11-16 16:35:57 +0200

    3. Refactor ShoppingCartItem and OrderItemListTile to use AsyncValueWidget for product data retrieval

--- ecommerce_app/lib/src/features/orders/presentation/order_list/order_item_list_tile.dart
+++ ecommerce_app/lib/src/features/orders/presentation/order_list/order_item_list_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../../common_widgets/async_value_widget.dart';
 import '../../../../common_widgets/custom_image.dart';
 import '../../../../constants/app_sizes.dart';
 import '../../../../localization/string_hardcoded.dart';
@@ -14,32 +15,34 @@ class OrderItemListTile extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final productsRepository = ref.watch(productsRepositoryProvider);
-    final product = productsRepository.getProduct(item.productId)!;
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: Sizes.p8),
-      child: Row(
-        children: [
-          Flexible(
-            flex: 1,
-            child: CustomImage(imageUrl: product.imageUrl),
-          ),
-          gapW8,
-          Flexible(
-            flex: 3,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(product.title),
-                gapH12,
-                Text(
-                  'Quantity: ${item.quantity}'.hardcoded,
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
+    final productStream = ref.watch(productStreamProvider(item.productId));
+    return AsyncValueWidget(
+      asyncValue: productStream,
+      data: (product) => Padding(
+        padding: const EdgeInsets.symmetric(vertical: Sizes.p8),
+        child: Row(
+          children: [
+            Flexible(
+              flex: 1,
+              child: CustomImage(imageUrl: product!.imageUrl),
             ),
-          ),
-        ],
+            gapW8,
+            Flexible(
+              flex: 3,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(product.title),
+                  gapH12,
+                  Text(
+                    'Quantity: ${item.quantity}'.hardcoded,
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
