d4bd2ca6a019ac618679edce9f69a8ae329cf2b9
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-11-25 15:15:50 +0200

    6. Add remaining tests for Futures & Streams:
    Working with groups and testing Futures & Streams

--- ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
+++ ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
@@ -21,5 +21,29 @@ void main() {
       final product = fakeProductsRepository.getProduct('100');
       expect(product, null);
     });
+
+    test('fetchProducts() returns the global list of products', () async {
+      final fakeProductsRepository = FakeProductsRepository();
+      final products = await fakeProductsRepository.fetchProducts();
+      expect(products, kTestProducts);
+    });
+
+    test('watchProducts() emits the global list of products', () {
+      final fakeProductsRepository = FakeProductsRepository();
+      final products = fakeProductsRepository.watchProducts();
+      expect(products, emits(kTestProducts));
+    });
+
+    test('watchProduct(1) emits the first product', () {
+      final fakeProductsRepository = FakeProductsRepository();
+      final product = fakeProductsRepository.watchProduct('1');
+      expect(product, emits(kTestProducts.first));
+    });
+
+    test('watchProduct(100) emits null', () {
+      final fakeProductsRepository = FakeProductsRepository();
+      final product = fakeProductsRepository.watchProduct('100');
+      expect(product, emits(null));
+    });
   });
 }
