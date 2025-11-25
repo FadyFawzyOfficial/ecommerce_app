ccf1a7f61edd5536363e0ae0ff918c3c8a2e4d10
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-11-25 14:12:20 +0200

    2. Write the first unit test + adding toString() and equality implementations

--- ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
+++ ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
@@ -1,7 +1,11 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('fake products repository ...', (tester) async {
-    // TODO: Implement test
+  test('getProducts() returns global list of products', () {
+    final fakeProductsRepository = FakeProductsRepository();
+    final products = fakeProductsRepository.getProducts();
+    expect(products, kTestProducts);
   });
 }
