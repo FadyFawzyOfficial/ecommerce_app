68b92ae0f83b25d0be44fa78e6647f17e1e71f62
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-11-25 14:58:34 +0200

    5. Refactor tests in fake_products_repository_test.dart to group related tests under 'FakeProductsRepository'

--- ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
+++ ecommerce_app/test/src/features/products/data/fake_products_repository_test.dart
@@ -3,21 +3,23 @@ import 'package:ecommerce_app/src/features/products/data/fake_products_repositor
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('getProducts() returns global list of products', () {
-    final fakeProductsRepository = FakeProductsRepository();
-    final products = fakeProductsRepository.getProducts();
-    expect(products, kTestProducts);
-  });
+  group('FakeProductsRepository', () {
+    test('getProducts() returns global list of products', () {
+      final fakeProductsRepository = FakeProductsRepository();
+      final products = fakeProductsRepository.getProducts();
+      expect(products, kTestProducts);
+    });
 
-  test('getProduct(1) returns the first product ', () {
-    final fakeProductsRepository = FakeProductsRepository();
-    final product = fakeProductsRepository.getProduct('1');
-    expect(product, kTestProducts.first);
-  });
+    test('getProduct(1) returns the first product ', () {
+      final fakeProductsRepository = FakeProductsRepository();
+      final product = fakeProductsRepository.getProduct('1');
+      expect(product, kTestProducts.first);
+    });
 
-  test('getProduct(100) returns null', () {
-    final fakeProductsRepository = FakeProductsRepository();
-    final product = fakeProductsRepository.getProduct('100');
-    expect(product, null);
+    test('getProduct(100) returns null', () {
+      final fakeProductsRepository = FakeProductsRepository();
+      final product = fakeProductsRepository.getProduct('100');
+      expect(product, null);
+    });
   });
 }
