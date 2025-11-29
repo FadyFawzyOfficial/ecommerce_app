699392d756451594c711afa5a2d570e7645b2d6b
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-11-29 15:43:07 +0200

    9. Start implementing tests for FakeAuthRepository like FakeProductsRepository

---
+++ ecommerce_app/test/src/features/authentication/data/fake_auth_repository_test.dart
@@ -0,0 +1,46 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  const testEmail = 'test@test.com';
+  const testPassword = 'password';
+  final testUser =
+      AppUser(uid: testEmail.split('').reversed.join(''), email: testEmail);
+
+  FakeAuthRepository buildFakeAuthRepository() =>
+      FakeAuthRepository(addDelay: false);
+
+  group('FakeAuthRepository', () {
+    test('currentUser is null', () {
+      final fakeAuthRepository = buildFakeAuthRepository();
+      expect(fakeAuthRepository.currentUser, null);
+      expect(fakeAuthRepository.authStateChanges, emits(null));
+    });
+
+    test('currentUser is not null after user is signed in', () async {
+      final fakeAuthRepository = buildFakeAuthRepository();
+      await fakeAuthRepository.signInWithEmailAndPassword(
+          testEmail, testPassword);
+      expect(fakeAuthRepository.currentUser, testUser);
+      expect(fakeAuthRepository.authStateChanges, emits(testUser));
+    });
+
+    test('currentUser is not null after user registration', () async {
+      final fakeAuthRepository = buildFakeAuthRepository();
+      await fakeAuthRepository.createUserWithEmailAndPassword(
+          testEmail, testPassword);
+      expect(fakeAuthRepository.currentUser, testUser);
+      expect(fakeAuthRepository.authStateChanges, emits(testUser));
+    });
+
+    test('currentUser is null after user is signed out', () async {
+      final fakeAuthRepository = buildFakeAuthRepository();
+      await fakeAuthRepository.signInWithEmailAndPassword(
+          testEmail, testPassword);
+      await fakeAuthRepository.signOut();
+      expect(fakeAuthRepository.currentUser, null);
+      expect(fakeAuthRepository.authStateChanges, emits(null));
+    });
+  });
+}
